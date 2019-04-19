3836bc06cde5045dc7e2950157c8b274294acd6f
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2019-04-18 19:43:56 -0700

    Standardize indentation

--- index.js
+++ index.js
@@ -12,55 +12,55 @@ const browsers = ['chrome', 'chrome_android', 'edge', 'firefox', 'ie', 'safari',
 function getNewStats() {
 	let stats = {};
 	browsers.forEach(browser => {
-	  stats[browser] = { all: 0, true: 0, null: 0, real: 0 }
+		stats[browser] = { all: 0, true: 0, null: 0, real: 0 }
 	});
 
 	const checkSupport = (supportData, type) => {
-	  if (!Array.isArray(supportData)) {
-	    supportData = [supportData];
-	  }
-	  return supportData.some(item => item.version_added === type || item.version_removed === type)
+		if (!Array.isArray(supportData)) {
+			supportData = [supportData];
+		}
+		return supportData.some(item => item.version_added === type || item.version_removed === type)
 	};
 
 	const processData = (data) => {
-	  if (data.support) {
-	    browsers.forEach(function(browser) {
-	      stats[browser].all++;
-	      let real_value = true;
-	      if (!data.support[browser]) {
-	        stats[browser].null++;
-	        real_value = false;
-	      } else {
-	        if (checkSupport(data.support[browser], null)) {
-	          stats[browser].null++;
-	          real_value = false;
-	        }
-	        if (checkSupport(data.support[browser], true)) {
-	          stats[browser].true++;
-	          real_value = false;
-	        }
-	      }
-	      if (real_value) {
-	        stats[browser].real++;
-	      }
-	    });
-	  }
+		if (data.support) {
+			browsers.forEach(function(browser) {
+				stats[browser].all++;
+				let real_value = true;
+				if (!data.support[browser]) {
+					stats[browser].null++;
+					real_value = false;
+				} else {
+					if (checkSupport(data.support[browser], null)) {
+						stats[browser].null++;
+						real_value = false;
+					}
+					if (checkSupport(data.support[browser], true)) {
+						stats[browser].true++;
+						real_value = false;
+					}
+				}
+				if (real_value) {
+					stats[browser].real++;
+				}
+			});
+		}
 	};
 
 	const iterateData = (data) => {
-	  for (let key in data) {
-	    if (key === '__compat') {
-	      processData(data[key]);
-	    } else {
-	      iterateData(data[key]);
-	    }
-	  }
+		for (let key in data) {
+			if (key === '__compat') {
+				processData(data[key]);
+			} else {
+				iterateData(data[key]);
+			}
+		}
 	};
 
 	for (let data in bcd) {
-	  if (!(data === 'browsers' || data === 'webextensions')) {
-	    iterateData(bcd[data]);
-	  }
+		if (!(data === 'browsers' || data === 'webextensions')) {
+			iterateData(bcd[data]);
+		}
 	}
 
 	stats_data[require('./node_modules/mdn-browser-compat-data/package.json').version] = stats;
