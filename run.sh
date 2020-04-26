69fb030f7e33ba29ae1443aea5a768cd2b7f9968
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-04-26 11:31:51 -0700

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+npm update
+node index.js
+git add bcd-stats.json
+git add package-lock.json
+git reset HEAD package.json
+git commit -m "Update to BCD $(npm view mdn-browser-compat-data version)"
+git push
