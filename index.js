6173e8a3602797318f5101831156fe14867d1738
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-11-22 16:08:20 -0800

    Convert getNewStats() to importable function

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 'use strict';
 
 var fs = require('fs');
-const bcd = require('@mdn/browser-compat-data');
-const bcdversion = require(require.resolve('@mdn/browser-compat-data/package.json')).version;
 
-const stats = require("./bcd-stats.json");
+const allbrowsers = ['chrome', 'chrome_android', 'edge', 'firefox', 'ie', 'safari', 'safari_ios', 'webview_android'];
 
-const browsers = ['chrome', 'chrome_android', 'edge', 'firefox', 'ie', 'safari', 'safari_ios', 'webview_android'];
-
-const getNewStats = () => {
-	// Reset stats
-	stats.all[bcdversion] = 0;
+const getStats = (bcd, browsers, folder) => {
+	let stats = {all: 0};
 	for (const browser of browsers) {
-		stats[browser].true[bcdversion] = 0;
-		stats[browser].null[bcdversion] = 0;
-		stats[browser].real[bcdversion] = 0;
+		stats[browser] = {true: 0, null: 0, real: 0};
 	}
 
 	const checkSupport = (supportData, type) => {
@@ -26,24 +19,24 @@ const getNewStats = () => {
 
 	const processData = (data) => {
 		if (data.support) {
-			stats.all[bcdversion]++;
+			stats.all++;
 			browsers.forEach(function(browser) {
 				let real_value = true;
 				if (!data.support[browser]) {
-					stats[browser].null[bcdversion]++;
+					stats[browser].null++;
 					real_value = false;
 				} else {
 					if (checkSupport(data.support[browser], null)) {
-						stats[browser].null[bcdversion]++;
+						stats[browser].null++;
 						real_value = false;
 					}
 					if (checkSupport(data.support[browser], true)) {
-						stats[browser].true[bcdversion]++;
+						stats[browser].true++;
 						real_value = false;
 					}
 				}
 				if (real_value) {
-					stats[browser].real[bcdversion]++;
+					stats[browser].real++;
 				}
 			});
 		}
@@ -59,15 +52,44 @@ const getNewStats = () => {
 		}
 	};
 
-	for (let data in bcd) {
-		if (!(data === 'browsers' || data === 'webextensions')) {
-			iterateData(bcd[data]);
+	if (folder) {
+		iterateData(bcd[folder]);
+	} else {
+		for (let data in bcd) {
+			if (!(data === 'browsers' || data === 'webextensions')) {
+				iterateData(bcd[data]);
+			}
 		}
 	}
+
+	return stats;
 }
 
-getNewStats();
+const main = () => {
+	const bcd = require('@mdn/browser-compat-data');
+	const bcdversion = require(require.resolve('@mdn/browser-compat-data/package.json')).version;
+	const stats = require("./bcd-stats.json");
 
-fs.writeFile("./bcd-stats.json", JSON.stringify(stats), function (err) {
-	if (err) return console.log(err);
-});
+	const newStats = getStats(bcd, allbrowsers, '');
+
+	for (const [key, data] of Object.entries(newStats)) {
+		if (key === 'all') {
+			stats.all[bcdversion] = data;
+			continue;
+		}
+
+		stats[key].true[bcdversion] = data.true;
+		stats[key].null[bcdversion] = data.null;
+		stats[key].real[bcdversion] = data.real;
+	}
+
+	fs.writeFile("./bcd-stats.json", JSON.stringify(stats), function (err) {
+		if (err) return console.log(err);
+	});
+}
+
+if (require.main === module) {
+	main();
+}
+
+module.exports = getStats;
